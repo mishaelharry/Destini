f6ba6ab3a9636a9a987278a74f571c95baeaf548
Mishael <mishael.harry@cyberspace.net.ng>
2018-03-02 12:21:04 +0100

    Initial commit

---
+++ app/src/main/java/com/app/destini/MainActivity.java
@@ -0,0 +1,43 @@
+package com.app.destini;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // TODO: Steps 4 & 8 - Declare member variables here:
+    private TextView mStoryTextView;
+    private Button mTopButton;
+    private Button mBottonButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
+        mStoryTextView = (TextView) findViewById(R.id.storyTextView);
+        mTopButton = (Button) findViewById(R.id.buttonTop);
+        mBottonButton = (Button) findViewById(R.id.buttonBottom);
+
+        // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
+        mTopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
+        mBottonButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+}
